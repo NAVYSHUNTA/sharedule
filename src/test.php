46093af3c0655443d5c8df7479e0f6ddf5f3ba03
Yuto TSUMAGARI <113566838+k21rs085@users.noreply.github.com>
2023-06-13 19:32:27 +0900

    Update and rename test3.php to test.php

--- src/test.php
+++ src/test.php
@@ -13,12 +13,13 @@
 <body>
   <!-- メニューバーの表示 -->
   <div class="navbar">
+    <!--このページは試作用です-->>
     <!--#にリンクを挿入-->
     <!--画像が挿入されているところは、ホームに遷移するためのボタンです-->
-    <a href="#" style="width: 80px; height: 30px;"><img src="images/logo.png" alt="メニュー1" /></a>
-    <a href="#" class="btn btn-flat"><span>課題</span></a>
-    <a href="#" class="btn btn-flat"><span>フレンドリスト</span></a>
-    <a href="#" class="btn btn-flat"><span>設定</span></a>
+    <a href="timeTable.php" style="width: 80px; height: 30px;"><img src="images/logo.png" alt="メニュー1" /></a>
+    <a href="theme.php" class="btn btn-flat"><span>課題</span></a>
+    <a href="friendList.php" class="btn btn-flat"><span>フレンドリスト</span></a>
+    <a href="setting.php" class="btn btn-flat"><span>設定</span></a>
   </div>
 </body>
 </html>
