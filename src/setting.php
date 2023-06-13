28a74fb2c5ef7194bb73663add819c43536ff77f
Yuto TSUMAGARI <113566838+k21rs085@users.noreply.github.com>
2023-06-13 19:32:00 +0900

    画面のタイトル名を追加した。

--- src/setting.php
+++ src/setting.php
@@ -15,10 +15,12 @@
   <div class="navbar">
     <!--#にリンクを挿入-->
     <!--画像が挿入されているところは、ホームに遷移するためのボタンです-->
-    <a href="#" style="width: 80px; height: 30px;"><img src="images/logo.png" alt="メニュー1" /></a>
-    <a href="#" class="btn btn-flat"><span>課題</span></a>
-    <a href="#" class="btn btn-flat"><span>フレンドリスト</span></a>
-    <a href="#" class="btn btn-flat"><span>設定</span></a>
+    <!--表題に試作としてわかりやすいように何のページか表しております（不必要であれば排除お願いします！）-->
+    <a href="timeTable.php" style="width: 80px; height: 30px;"><img src="images/logo.png" alt="メニュー1" /></a>
+    <a href="theme.php" class="btn btn-flat"><span>課題</span></a>
+    <a href="friendList.php" class="btn btn-flat"><span>フレンドリスト</span></a>
+    <a href="setting.php" class="btn btn-flat"><span>設定</span></a>
+    <h1>設定</h1>
   </div>
 </body>
 </html>
