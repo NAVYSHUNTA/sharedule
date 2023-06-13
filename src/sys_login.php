79361c76e47bf5bd112c44d28efb1728e8c203a1
Yuto TSUMAGARI <113566838+k21rs085@users.noreply.github.com>
2023-06-13 21:57:37 +0900

    ログイン画面のデザインを設計した。

--- src/sys_login.php
+++ src/sys_login.php
@@ -7,17 +7,43 @@
         display: flex;
         justify-content: center;
         align-items: center;
-        height: 100vh;
+        height: 60vh;
+        flex-direction: column;
+    }
+
+    .input-container {
+        text-align: center;
+    }
+
+    .button-container {
+        text-align: center;
     }
 </style>
 </head>
 <body>
 <form action="wp06check.php" method="post">
     <table>
-        <tr><td>ユーザ名：</td><td><input type="text" name="uid"></td></tr>
-        <tr><td>パスワード：</td><td><input type="password" name="pass"></td></tr>
+        <tr>
+            <td colspan="2">
+                <div class="image-container">
+                    <img src="images/logo.png">
+                </div>
+            </td>
+        </tr>
+        <tr>
+            <td class="input-container">ユーザ名：</td>
+            <td><input type="text" name="uid"></td>
+        </tr>
+        <tr>
+            <td class="input-container">パスワード：</td>
+            <td><input type="password" name="pass"></td>
+        </tr>
     </table>
-    <input type="submit" value="送信"><input type="reset" value="取消">
+    <div class="button-container">
+        <input type="submit" value="登録">
+        <input type="submit" value="ログイン">
+        <!--ボタンの機能はまだ触ってないです-->
+    </div>
 </form>
 </body>
 </html>
