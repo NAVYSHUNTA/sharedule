a5ba5751d6d4a1aa1bc8a5b3a3570ef2b2437451
Yuuto Tsumagari <k21rs085@st.kyusan-u.ac.jp>
2023-06-14 18:54:32 +0900

    index.phpからログイン画面への画面遷移を記述した。また、ログイン画面にロゴが表示されないバグを解消した。

--- index.php
+++ index.php
@@ -1,10 +1,9 @@
 <?php
 session_start();
 
-$action = '\sys_login'; //ホームページ (eps_home)をデフォルト機能とする
+$action = '\src\sys_login'; //ホームページ (eps_home)をデフォルト機能とする
 if (isset($_GET['do'])) {//index.php?do=に続くパラメータで実行する機能を指定
   $action = $_GET['do'];
 }
-include('C:\xampp82\htdocs\sharedule\src' . $action . '.php'); //指定されたファイルを読み込む
-
+include(dirname(__FILE__) . $action . '.php'); //指定されたファイルを読み込む
 ?>
