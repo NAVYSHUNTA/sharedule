d4e1d39af4c3a9415ace486c2d07af6dc7130c1a
Shunta Nakamura <52842964+NAVYSHUNTA@users.noreply.github.com>
2023-06-14 22:10:23 +0900

    index.phpが実行されないエラーを解消

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 
-$action = '\src\sys_login'; //ホームページ (eps_home)をデフォルト機能とする
+$action = '/src/sys_login'; //ホームページ (eps_home)をデフォルト機能とする
 if (isset($_GET['do'])) {//index.php?do=に続くパラメータで実行する機能を指定
   $action = $_GET['do'];
 }
