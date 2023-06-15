cdf4b274ecd61d6d08836828f5ce0b92e9dcd665
nakagaki itsuki <k21rs089@st.kyusan-u.ac.jp>
2023-06-15 20:09:22 +0900

    課題画面のレイアウト変更

--- src/theme.php
+++ src/theme.php
@@ -10,6 +10,7 @@
 
     <title>sharedule</title>
 </head>
+
 <body>
   <!-- メニューバーの表示 -->
   <div class="navbar">
@@ -22,5 +23,147 @@
     <a class="btn btn-flat"><span>福岡　太郎</span></a>
     <a href="sys_login.php" class="btn btn-flat"><span>ログアウト</span></a>
   </div>
+  <style>
+    /* 枠線のスタイル */
+    table {
+      position: relative;
+      top: 200px;
+      left:28%;
+      border-collapse: collapse;
+      margin-bottom: 20px;
+    }
+
+    th, td {
+      width: 100px;
+      height: 100px;
+      border: 1px solid black;
+      padding: 10px;
+      text-align: center;
+    }
+
+    .button-container {
+      margin-bottom: 20px;
+    }
+
+    .button-container button {
+      position: relative;
+      left:40%;
+      top: 380px;
+      margin-right: 10px;
+    }
+  </style>
+</head>
+<body>
+
+  <table id="table1" class="table">
+    <tbody>
+      <tr>
+        <th>6/15</th>
+        <th>6/16</th>
+        <th>6/17</th>
+        <th>6/18</th>
+        <th>6/19</th>
+        <th>6/20</th>
+        <th>6/21</th>
+      </tr>
+      <tr>
+        <td>課題</td>
+        <td>課題</td>
+        <td>課題</td>
+        <td>課題</td>
+        <td>課題</td>
+        <td>課題</td>
+        <td>課題</td>
+      </tr>
+    </tbody>
+  </table>
+
+  <table id="table2" class="table" style="display: none;">
+  <tbody>
+      <tr>
+        <th>6/22</th>
+        <th>6/23</th>
+        <th>6/24</th>
+        <th>6/25</th>
+        <th>6/26</th>
+        <th>6/27</th>
+        <th>6/28</th>
+      </tr>
+      <tr>
+        <td>課題</td>
+        <td>課題</td>
+        <td>課題</td>
+        <td>課題</td>
+        <td>課題</td>
+        <td>課題</td>
+        <td>課題</td>
+      </tr>
+    </tbody>
+  </table>
+
+  <table id="table3" class="table" style="display: none;">
+  <tbody>
+      <tr>
+        <th>6/29</th>
+        <th>6/30</th>
+        <th>7/1</th>
+        <th>7/2</th>
+        <th>7/3</th>
+        <th>7/4</th>
+        <th>7/5</th>
+      </tr>
+      <tr>
+        <td>課題</td>
+        <td>課題</td>
+        <td>課題</td>
+        <td>課題</td>
+        <td>課題</td>
+        <td>課題</td>
+        <td>課題</td>
+      </tr>
+    </tbody>
+  </table>
+
+  <table id="table4" class="table" style="display: none;">
+  <tbody>
+      <tr>
+        <th>7/6</th>
+        <th>7/7</th>
+        <th>7/8</th>
+        <th>7/9</th>
+        <th>7/10</th>
+        <th>7/11</th>
+        <th>7/12</th>
+      </tr>
+      <tr>
+        <td>課題</td>
+        <td>課題</td>
+        <td>課題</td>
+        <td>課題</td>
+        <td>課題</td>
+        <td>課題</td>
+        <td>課題</td>
+      </tr>
+    </tbody>
+  </table>
+<div class="button-container">
+    <button onclick="toggleTable(1)">先週</button>
+    <button onclick="toggleTable(2)">今週</button>
+    <button onclick="toggleTable(3)">来週</button>
+    <button onclick="toggleTable(4)">再来週</button>
+  </div>
+  <script>
+    // JavaScriptコード
+    var tables = document.querySelectorAll(".table");
+    var currentTable = 1;
+
+    function toggleTable(tableNumber) {
+      if (tableNumber >= 1 && tableNumber <= 4 && tableNumber !== currentTable) {
+        tables[currentTable - 1].style.display = "none";
+        tables[tableNumber - 1].style.display = "table";
+        currentTable = tableNumber;
+      }
+    }
+  </script>
 </body>
 </html>
