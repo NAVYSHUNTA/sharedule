cf377d4ffaf1ed4e15056ff79d522a0c0452f344
nekoy3 <nekoyama3.n@gmail.com>
2023-06-16 20:48:36 +0900

    パス訂正

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 
-$action = '/src/sys_login'; //ホームページ (eps_home)をデフォルト機能とする
+$action = '/sys_login'; //ホームページ (eps_home)をデフォルト機能とする
 if (isset($_GET['do'])) {//index.php?do=に続くパラメータで実行する機能を指定
   $action = $_GET['do'];
 }
