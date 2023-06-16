d8fb0ce3b5113377029264163a7cbea6b31d4fb9
nekoy3 <nekoyama3.n@gmail.com>
2023-06-16 21:27:11 +0900

    取り消し

--- src/sys_login.php
+++ src/sys_login.php
@@ -29,7 +29,7 @@
         unset($_SESSION['login_error']); // エラーメッセージを表示後に削除する
     }
     ?>
-    <form action="sys_check" method="post">
+    <form action="?do=sys_check" method="post">
         <table>
             <tr>
                 <td colspan="2">
