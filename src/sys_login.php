877374c10e51305a9d36ac38b9c896ea0e7ed5c4
Yuuto Tsumagari <k21rs085@st.kyusan-u.ac.jp>
2023-06-16 21:43:32 +0900

    ログイン画面の見栄えを変更しました。

--- src/sys_login.php
+++ src/sys_login.php
@@ -7,7 +7,7 @@
         display: flex;
         justify-content: center;
         align-items: center;
-        height: 60vh;
+        height: 100vh; /* 画面全体の高さに設定 */
         flex-direction: column;
     }
 
@@ -17,7 +17,25 @@
 
     .button-container {
         text-align: center;
+        margin-top: 40px; /* ボタンの上部マージンを追加 */
     }
+
+    .custom-button {
+        background-color:#000a09 ;
+        color: #ffffff;
+        font-size: 24px; /* ボタンの文字サイズを変更 */
+        padding: 10px 20px; /* ボタンのパディングを調整 */
+        border-radius: 7px; /* ボタンの角丸を設定 */
+    }
+    .custom-button2{
+        background-color: #000a09;
+        color: #ffffff;
+        font-size: 24px; /* ボタンの文字サイズを変更 */
+        padding: 10px 8px; /* ボタンのパディングを調整 */
+        border-radius: 7px; /* ボタンの角丸を設定 */  
+
+    }
+
 </style>
 </head>
 <body>
@@ -32,16 +50,16 @@
         </tr>
         <tr>
             <td class="input-container">ユーザ名：</td>
-            <td><input type="text" name="uid"></td>
+            <td><input type="text" name="username" placeholder="ユーザ名" style="width: 200px; height: 25px;"></td>
         </tr>
         <tr>
             <td class="input-container">パスワード：</td>
-            <td><input type="password" name="pass"></td>
+            <td><input type="password" name="password"  placeholder="パスワード" style="width:200px; height: 30px;"></td>
         </tr>
     </table>
     <div class="button-container">
-        <input type="submit" value="登録">
-        <input type="submit" value="ログイン">
+        <input type="submit" value="登録" class="custom-button">
+        <input type="submit" value="ログイン"class="custom-button2">
         <!--ボタンの機能はまだ触ってないです-->
     </div>
 </form>
