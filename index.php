906d93dcbddd42dea84b6ee4331526146b9800c2
nekoy3 <nekoyama3.n@gmail.com>
2023-06-17 12:09:47 +0900

    ページごとにページ名を設定する

--- index.php
+++ index.php
@@ -4,15 +4,33 @@ ini_set('display_errors', 1);
 session_start();
 
 $action = 'sys_login'; //ホームページ (eps_home)をデフォルト機能とする
+$_SESSION['now_page'] = ""; //ページ名
 
 if ($_SESSION['uid']) {
-  include('src/pg_header.php'); //ヘッダー部分を読み込む
   $action = 'timeTable'; //セッションが存在すれば初期ページをtimeTable.phpとする
 }
 
 if (isset($_GET['do'])) {//index.php?do=に続くパラメータで実行する機能を指定
   $action = $_GET['do'];
+  switch ($action) {
+    case "friendList":
+      $_SESSION['now_page'] = "フレンドリスト";
+      break;
+
+    case "setting":
+      $_SESSION['now_page'] = "設定ページ";
+      break;
+
+    case "theme":
+      $_SESSION['now_page'] = "課題一覧";
+      break;
+
+    case "timeTable":
+      $_SESSION['now_page'] = "時間割";
+      break;
+  }
 }
 
+include('src/pg_header.php'); //ヘッダー部分を読み込む
 include('src/' . $action . '.php'); //指定されたファイルを読み込む
 ?>
