77245766e4eacd43af81e8ec4592d705025cf7e6
nekoy3 <nekoyama3.n@gmail.com>
2023-06-17 13:52:03 +0900

    場所変更

--- src/timeTable.php
+++ src/timeTable.php
@@ -45,23 +45,6 @@
 </head>
 
 <body>
-  <h1>授業科目登録フォーム</h1>
-  <form method="post" action="register.php">
-    <label for="subject">科目名:</label>
-    <input type="text" name="subject" id="subject" required><br><br>
-
-    <label for="day">曜日:</label>
-    <input type="text" name="day" id="day" maxlength="1" required><br><br>
-
-    <label for="period">時限:</label>
-    <input type="number" name="period" id="period" required><br><br>
-
-    <label for="classroom_number">教室番号:</label>
-    <input type="number" name="classroom_number" id="classroom_number" required><br><br>
-
-    <input type="submit" value="登録">
-  </form>
-
   <table id="myTable">
     <tr>
       <th> </th>
@@ -118,6 +101,23 @@
       <td>Sat-5</td>
     </tr>
   </table>
+
+  <h2>授業科目登録フォーム</h2>
+  <form method="post" action="?do=regist_subject">
+    <label for="subject">科目名:</label>
+    <input type="text" name="subject" id="subject" required><br><br>
+
+    <label for="day">曜日:</label>
+    <input type="text" name="day" id="day" maxlength="1" required><br><br>
+
+    <label for="period">時限:</label>
+    <input type="number" name="period" id="period" required><br><br>
+
+    <label for="classroom_number">教室番号:</label>
+    <input type="number" name="classroom_number" id="classroom_number" required><br><br>
+
+    <input type="submit" value="登録">
+  </form>
 
   <div id="listContainer" class="list"></div>
 
