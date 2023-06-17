d2abb8afdf7cfdb86ba863458014bd6ed852656f
nekoy3 <nekoyama3.n@gmail.com>
2023-06-17 11:46:04 +0900

    読み込み準変更

--- index.php
+++ index.php
@@ -4,12 +4,13 @@ ini_set('display_errors', 1);
 session_start();
 
 $action = 'sys_login'; //ホームページ (eps_home)をデフォルト機能とする
-if (isset($_GET['do'])) {//index.php?do=に続くパラメータで実行する機能を指定
-  $action = $_GET['do'];
-}
 
 if ($_SESSION['uid']) {
   $action = 'timeTable';
+}
+
+if (isset($_GET['do'])) {//index.php?do=に続くパラメータで実行する機能を指定
+  $action = $_GET['do'];
 }
 
 include('src/' . $action . '.php'); //指定されたファイルを読み込む
