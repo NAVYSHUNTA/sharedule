667922934f6cdb84fbc4309a3b920fe3e899adee
nekoy3 <nekoyama3.n@gmail.com>
2023-06-17 12:37:12 +0900

    ログアウトページの実装

--- src/sys_logout.php
+++ src/sys_logout.php
@@ -16,49 +16,61 @@
             text-align: center;
         }
 
-    .button-container {
-        text-align: center;
-        margin-top: 40px; /* ボタンの上部マージンを追加 */
-    }
+        .button-container {
+            text-align: center;
+            margin-top: 40px;
+            /* ボタンの上部マージンを追加 */
+        }
 
-    .custom-button {
-        background-color:#000a09 ;
-        color: #ffffff;
-        font-size: 24px; /* ボタンの文字サイズを変更 */
-        padding: 10px 20px; /* ボタンのパディングを調整 */
-        border-radius: 7px; /* ボタンの角丸を設定 */
-    }
-    .custom-button2{
-        background-color: #000a09;
-        color: #ffffff;
-        font-size: 24px; /* ボタンの文字サイズを変更 */
-        padding: 10px 8px; /* ボタンのパディングを調整 */
-        border-radius: 7px; /* ボタンの角丸を設定 */  
+        .custom-button {
+            background-color: #000a09;
+            color: #ffffff;
+            font-size: 24px;
+            /* ボタンの文字サイズを変更 */
+            padding: 10px 20px;
+            /* ボタンのパディングを調整 */
+            border-radius: 7px;
+            /* ボタンの角丸を設定 */
+        }
 
-    }
+        .custom-button2 {
+            background-color: #000a09;
+            color: #ffffff;
+            font-size: 24px;
+            /* ボタンの文字サイズを変更 */
+            padding: 10px 8px;
+            /* ボタンのパディングを調整 */
+            border-radius: 7px;
+            /* ボタンの角丸を設定 */
 
-</style>
+        }
+    </style>
 </head>
 
 <body>
     <form action="?do=goodbye" method="post">
         <table>
-            
+
             <tr>
                 <td colspan="2">
                     <div class="image-container">
                         <img src="./images/logo.png">
-                </div>
-            </td>
-        </tr>
-    </table>
-    
-    <div class="button-container">
-        <button type="submit" name="btn" value="register" class="custom-button">登録</button>
-        <button type="submit" name="btn" value="login"class="custom-button2">ログイン</button>
-        <!--ボタンの機能はまだ触ってないです-->
-    </div>
-</form>
+                    </div>
+                </td>
+            </tr>
+        </table>
+
+        <?php
+        if (isset($_POST['logout'])) {
+            header("Location: ?do=goodbye");
+        }
+        ?>
+
+        <p>ログアウトしますか？</p>
+        <div class="button-container">
+            <button type="submit" name="logout">はい</button>
+        </div>
+    </form>
 </body>
 
 </html>
