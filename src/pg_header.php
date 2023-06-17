253bac58b32edb61fa3745d6ac9420c3ebae7308
nekoy3 <nekoyama3.n@gmail.com>
2023-06-17 11:59:57 +0900

    ヘッダー単体の実装

--- src/pg_header.php
+++ src/pg_header.php
@@ -10,43 +10,15 @@
 <body>
   <div class="wrapper">
     <div id="navbar">
-      <a href="timeTable.php" style="width: 80px; height: 30px;"><img src="./images/logo.png" alt="メニュー1" /></a>
-      <a href="theme.php" class="btn btn-flat"><span>課題</span></a>
-      <a href="friendList.php" class="btn btn-flat"><span>フレンドリスト</span></a>
-      <a href="setting.php" class="btn btn-flat"><span>設定</span></a>
-      <a class="btn btn-flat"><span>福岡　太郎</span></a>
-      <a href="sys_login.php" class="btn btn-flat"><span>ログアウト</span></a>
+      <a href="?do=timeTable" style="width: 80px; height: 30px;"><img src="./images/logo.png" alt="メニュー1" /></a>
+      <a href="?do=theme" class="btn btn-flat"><span>課題</span></a>
+      <a href="?do=friendList" class="btn btn-flat"><span>フレンドリスト</span></a>
+      <a href="?do=setting" class="btn btn-flat"><span>設定</span></a>
       <?php
-      if (isset($_SESSION['urole'])) {
-        echo  $_SESSION['uname'] . '&nbsp';
-        echo '[<a href="?do=eps_home">HOME</a>]&nbsp';
-        $menu = array();
-        if ($_SESSION['urole'] == 1) {  //学生
-          $menu = array(   //学生メニュー
-            '成績確認'  => 'eps_grade',
-            '希望登録'  => 'eps_submit',
-            '配属結果'  => 'eps_result',
-          );
-        }
-        if ($_SESSION['urole'] == 2) { //教員
-          $menu = array(   //教員メニュー
-            '希望一覧・配属決定'  => 'eps_list',
-            '未登録者'  => 'eps_noentry',
-            '希望集計'  => 'eps_summary',
-          );
-        }
-        if ($_SESSION['urole'] == 9) { //管理者
-          $menu = array(   //教員メニュー
-            'アカウント一覧'  => 'usr_list',
-          );
-        }
-        foreach ($menu as $label => $action) {
-          echo  '[<a href="?do=' . $action . '">' . $label . '</a>]&nbsp;';
-        }
-        echo  '[<a href="?do=sys_logout">ログアウト</a>]&nbsp;';
-      } else {
-        echo  '[<a href="?do=sys_login">ログイン</a>]';
-      }
+        echo "<a class=\"btn btn-flat\"><span>{$_SESSION['uname']}</span></a>";
       ?>
+      <a href="?do=sys_logout">ログアウト</a>
     </div>
-    <h2>sharedule</h2>
+    <?php
+      echo "<h1>{$_SESSION['now_page']}</h1>";
+    ?>
